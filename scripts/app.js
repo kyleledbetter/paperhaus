36e9deb373ffa88e67a36ac3b4944db235ff7390
kyleledbetter <kyle@kyleledbetter.com>
2016-04-23 16:05:47 -0500

    update(theme)

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('RadioActive', ['ngMaterial', 'ui.router']);
+var app = angular.module('Paper', ['ngMaterial', 'ui.router']);
 
 app.run(['$rootScope', '$state', '$stateParams',
   function($rootScope, $state, $stateParams) {
@@ -91,7 +91,7 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
       desc: 'The idea is to create an app that the marketers can use by extending the customer influencer index model that calculates customers influence based on relations',
       rows: 2,
       columns: 2,
-      background: 'bgc-teal-A700',
+      background: 'bgc-teal-600',
       image: 'img/full/material-design-events.png'
     },
     {
@@ -164,19 +164,6 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
     });
   };
 
-  $scope.showAdd = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      template: '<md-dialog aria-label="Form"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="user.lastName"> </md-input-container> </div> <md-input-container flex> <label>Message</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
-      targetEvent: ev,
-    })
-    .then(function(answer) {
-      $scope.alert = 'You said the information was';
-    }, function() {
-
-      $scope.alert = 'You cancelled the dialog.';
-    });
-  };
 }]);
 
 app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
@@ -194,37 +181,21 @@ app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
 });
 
 app.config(function($mdThemingProvider) {
-  $mdThemingProvider.definePalette('customPrimary', {
-    '50': 'FFF9C4',
-    '100': 'FFF8E1',
-    '200': 'ef9a9a',
-    '300': 'e57373',
-    '400': 'ef5350',
-    '500': 'f44336',
-    '600': 'e53935',
-    '700': 'd32f2f',
-    '800': 'c62828',
-    '900': 'b71c1c',
-    'A100': 'ff8a80',
-    'A200': 'ff5252',
-    'A400': 'ff1744',
-    'A700': 'd50000',
-    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-                                        // on this palette should be dark or light
-    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-     '200', '300', '400', 'A100'],
-    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+  var primaryMap = $mdThemingProvider.extendPalette('deep-purple', {
+    '50': '#ffffff',
+    'contrastDefaultColor': 'light'
   });
+  $mdThemingProvider.definePalette('customPrimary', primaryMap);
 
   $mdThemingProvider.theme('default')
-    .primaryPalette('grey', {
-      'default': '200',
+    .primaryPalette('customPrimary', {
+      'default': '800',
       'hue-1': '50',
-      'hue-2': '100'
+      'hue-2': '900'
     })
     .accentPalette('pink', {
       'default': 'A400',
-      'hue-1': '50',
+      'hue-1': 'A400',
     })
     .backgroundPalette('blue-grey', {
       'default': '50'
